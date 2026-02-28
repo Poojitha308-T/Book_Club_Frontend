e49baa258634c6a49bb6ee359d2bf8c9acb586ef
Poojitha308-T <poojithathadiboyina@gmail.com>
2026-03-01 04:07:12 +0530

    Fix Vercel SPA routing

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,7 +6,7 @@ const api = axios.create({
 });
 
 export const registerUser = async (data) => {
-  const response = await API.post("/auth/register", data);
+  const response = await api.post("/auth/register", data);
   return response.data;
 };
 
